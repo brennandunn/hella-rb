a57e44ea58dd2deb695045e348fd251cfeed7d33
Brennan Dunn <me@brennandunn.com>
2008-06-29 14:19:36 -0400

    Added tasks to interactive

--- lib/hella/interactive.rb
+++ lib/hella/interactive.rb
@@ -8,9 +8,32 @@ $interactive = true
 $opts = options do
   version "hella-rb #{Hella::Version.to_s}"
   opt :config, 'Configuration file', :default => File.expand_path('~/.hellarb')
+  opt :tasks, 'List tasks'
 end
 
-cmd = ARGV.shift or die 'expecting a command'
+if $opts[:tasks]
+  puts <<-EOS
+hella-rb #{Hella::Version.to_s}
+------------------------
+Usage: hellarb <command> <optional arguments>
+
+Commands:
+  rc            Writes configuration file to #{File.expand_path('~/.hellarb')}
+  log           List log entries produced by Hellanzb
+  cancel        Cancel downloads
+  pause         Pause downloads
+  resume        Resume downloads
+  toggle        Pause if Hellanzb is active, resume if paused
+  queue:size    Amount of MB queued for download
+  queue:add     Space seperated list of newzbin IDs. e.g. queue:add 12345 67890
+  system        Call directly to Hellanzb without going through hella-rb
+  
+More information can be found at http://www.brennandunn.com  
+  
+  EOS
+end
+
+cmd = ARGV.shift or die 'expecting a command' unless $opts[:tasks]
 hella = Hella.connect
 hella.configuration[:dotfile] = $opts[:config]
 
